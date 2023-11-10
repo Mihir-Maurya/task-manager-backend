36b3c1b4cf66240098e8a3fe65c1ae6df12883e2
Mihir <email.com>
2023-11-10 17:38:00 +0530

    update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({}))
 app.use("/",userRouter)
 app.use("/api/tasks",taskrouter);
