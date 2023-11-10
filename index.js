473038534b8c4504aa739ecf3199758b05351c45
Mihir <email.com>
2023-11-10 17:49:50 +0530

    commit

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ app.use("/api/tasks",taskrouter);
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT =  5000;
 
 const startServer = async () => {
   try {
     await connectDB();
 
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+      console.log(`Server  on port ${PORT}`);
     });
   } catch (error) {
     console.log(error);
